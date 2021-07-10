b9d93f55ee90a123d615e8ba6a33d5aae44e96c6
三富隼斗 <mitomihayato@mitomihayatonoMacBook-Pro.local>
2021-07-10 14:30:56 +0900

    スタイルを適用する

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,32 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-let title = "Next.js page";
-let message = "React Next.js sample page";
 
 export default function Home() {
+
+  let title = "Next.js page";
+  let message = "React Next.js sample page";
+
+  const h1 = {
+    textAlign: 'right',
+    padding: '5px 15px'
+  };
+
+  const p = {
+    textAlign: 'left',
+    margin: '0px 5px',
+    color: '#669',
+    fontSize: '18pt'
+  }
+
+  const subtitle = {
+    textAlign: 'center',
+    margin: '0px 5px',
+    color: '#99d',
+    fontSize: '24pt',
+    fontWeight: 'bold'
+  };
+
   return (
     <div>
       <Head>
@@ -13,11 +35,11 @@ export default function Home() {
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" crossOrigin="anonymous" />
       </Head>
 
-      <h1 className="bg-primary text-white display-4">React</h1>
+      <h1 className="bg-primary text-white display-4" style={h1}>React</h1>
       <div className="container">
-        <h4 className="my-3">{title}</h4>
+        <p className="my-3" style={subtitle} id="subtitle">{title}</p>
         <div className="alert alert-primary text-center">
-          <p className="h5">{message}</p>
+          <p className="h5" style={p}>{message}</p>
         </div>
       </div>
     </div>
