bba1e072a5e017a4525132de1440357236570dc1
三富隼斗 <mitomihayato@mitomihayatonoMacBook-Pro.local>
2021-07-10 19:25:20 +0900

    useSWRでステートを用意する

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,31 @@
 import Layout from '../components/layout';
-import { useState } from 'react';
+import useSWR from 'swr';
 
 
 export default function Home() {
 
-  const url = './data.json';
-  const [data, setData] = useState({message: '', data: []});
-
-  fetch(url).then(res => res.json()).then(res => setData(res));
+  const { data } = useSWR('./data.json');
 
   return (
     <div>
       <Layout header="Next.js" title="Top page." >
         <div className="alert alert-primary text-center">
-          <h5 className="mb-4">{data.message}</h5>
+          <h5 className="mb-4">{data !== undefined ? data.message : 'error...'}</h5>
           <table className="table bg-white">
             <thead className="table-dark">
               <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
             </thead>
             <tbody>
-              {data.data.map((value, key) => (
+              {data !== undefined ?
+              data.data.map((value, key) => (
                 <tr key={key}>
                   <th>{value.name}</th>
                   <th>{value.mail}</th>
                   <th>{value.age}</th>
                 </tr>
-              ))}
+              ))
+            : <tr><th></th><td>no data.</td><td></td></tr>
+            }
             </tbody>
           </table>
         </div>
